39aaa59efa8bc555ad1aa565b923aa94faee53b5
Allvess <patrickalves930@gmail.com>
2020-01-15 10:37:10 -0300

    Criar Lista

---
+++ ListaF.java
@@ -0,0 +1,11 @@
+public class ListaF {
+
+    import java.util.ArrayList;
+    import java.util.List;
+    import java.util.Scanner;
+
+    public static void main(String[] args) {
+        String[] funcionarios = new String[]
+
+    }
+}
